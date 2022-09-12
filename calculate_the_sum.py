424849faef5a995e93da15f13630ef1b24cb10f6
alex-williams <alex.williams@chetwood.co>
2022-09-12 09:38:35 +0100

    Calculating the sum of evens in a list.

--- calculate_the_sum.py
+++ calculate_the_sum.py
@@ -1,5 +1,10 @@
 """calculate_the_sum.py"""
 
+
 def read_numbers_from_file():
     file = open("file.txt")
     return list(file.read())
+
+
+def calculate_the_sum_of_evens_in_a_list(list_of_numbers):
+    return sum([n for n in list_of_numbers if not n % 2])
